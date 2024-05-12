fbe4f6a095a76f989db1438baaf6e9f5af9718f8
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2024-05-12 12:20:20 +0545

    next version 2

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -74,5 +74,5 @@ router.delete("/:id", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
-//comments added
+//comments added to the next version
 module.exports = router;
